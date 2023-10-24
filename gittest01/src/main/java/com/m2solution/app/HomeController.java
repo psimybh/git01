1223cf3925bc440613f8656bd81967690ae0bdcf
simybh <simybh@simybh-TFX4150H>
2023-10-24 18:55:01 +0900

    2222

--- gittest01/src/main/java/com/m2solution/app/HomeController.java
+++ gittest01/src/main/java/com/m2solution/app/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime1", formattedDate );
 		
 		return "home";
 	}
